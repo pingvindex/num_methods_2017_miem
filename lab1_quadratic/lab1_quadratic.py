6a92f76c2cf648ee77d5ae82e835bdf288adedfb
Damir Zhadikov <d.zhadikov@gmail.com>
2017-01-13 14:39:12 +0300

    used Viet theorem

--- lab1_quadratic/lab1_quadratic.py
+++ lab1_quadratic/lab1_quadratic.py
@@ -6,17 +6,27 @@ def discriminant(b, c):
 
 
 def root_to_str(x):
+    if x[0] == 0.0 and x[1] == 0.0:
+        return '0'
+    if x[0] == 0.0:
+        return '{:.10f}j'.format(x[1])
+    if x[1] == 0.0:
+        return '{:.10f}'.format(x[0])
     return '{:+.10f}{:+.10f}j'.format(x[0], x[1])
 
 
 def solve_quadratic(b, c):
     D = discriminant(b, c)
     if D >= 0:
-        x1 = ((-b + D**0.5) / 2, 0.0)
-        x2 = ((-b - D**0.5) / 2, 0.0)
+        if b >= 0:
+            x2 = ((-b - D**0.5) / 2, 0.0)
+            x1 = (c / x2[0], 0.0)
+        else:
+            x1 = ((-b + D**0.5) / 2, 0.0)
+            x2 = (c / x1[0], 0.0)
     else:
-        x1 = (-b / 2, (-D)**0.5 / 2)
-        x2 = (-b / 2, -(-D)**0.5 / 2)
+        x1 = ((-b) / 2, (-D)**0.5 / 2)
+        x2 = ((-b) / 2, -(-D)**0.5 / 2)
     return root_to_str(x1)+' '+root_to_str(x2)
 
 
