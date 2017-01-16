3e2f88843ade126eeb52626566b0d2d5049e278c
Damir Zhadikov <d.zhadikov@gmail.com>
2017-01-16 23:39:52 +0300

    hope right format of printing

--- lab1_mach_arith/lab1_eps_inf_zero.py
+++ lab1_mach_arith/lab1_eps_inf_zero.py
@@ -2,15 +2,31 @@
 from __future__ import division, absolute_import, print_function
 
 def epsilon():
-    # ENTER YOUR CODE HERE
+    N = 0
+    while True:
+        try:
+            tmp = 2.0**N
+        except OverflowError:
+            return 2.0**(N-1)
+        N += 1
 
 
 def zero():
-    # ENTER YOUR CODE HERE
+    N = 0
+    while True:
+        if 2.0**(-N) == 0:
+            return 2**(-N+1)
+        else:
+            N+=1
 
 
 def infty():
-    # ENTER YOUR CODE HERE
+    N = 0
+    while True:
+        if 1 + 2.0**(-N) == 1:
+            return 2**(-N+1)
+        else:
+            N+=1
 
 
 if __name__ == "__main__":
--- lab1_quadratic/lab1_quadratic_complex.py
+++ lab1_quadratic/lab1_quadratic_complex.py
@@ -5,19 +5,33 @@ def discriminant(b, c):
     return b**2 - 4*c
 
 
+def to_str(x):
+    if (abs(x[0]) < 1e-10) and (abs(x[1]) < 1e-10):
+        return '{:.10f}'.format(0) 
+    elif (abs(x[1]) < 1e-10):
+        return '{:.10f}'.format(x[0])
+    elif (abs(x[0]) < 1e-10):
+        return '{:.10f}j'.format(x[1])
+    else:
+        if x[1] > 0:
+            return '{:.10f}+{:.10f}j'.format(x[0], x[1])
+        else:
+            return '{:.10f}{:.10f}j'.format(x[0], x[1])
+    
+
 def solve_quadratic(b, c):
     D = discriminant(b, c)
     if D >= 0:
         if b >= 0:
-            x2 = (-b - D**0.5) / 2
-            x1 = c / x2
+            x2 = ((-b - D**0.5) / 2, 0)
+            x1 = ((c / x2[0]), 0)
         else:
-            x1 = (-b + D**0.5) / 2
-            x2 = c / x1
+            x1 = ((-b + D**0.5) / 2, 0)
+            x2 = ((c / x1[0]), 0)
     else:
-        x1 = complex((-b) / 2, (-D)**0.5 / 2)
-        x2 = complex((-b) / 2, -(-D)**0.5 / 2)
-    return str(x1).strip('(').strip(')')+' '+str(x2).strip('(').strip(')')
+        x1 = ((-b) / 2, (-D)**0.5 / 2)
+        x2 = ((-b) / 2, -(-D)**0.5 / 2)
+    return to_str(x1)+' '+to_str(x2)
 
 
 if __name__ == "__main__":
